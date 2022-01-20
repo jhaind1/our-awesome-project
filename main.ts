94740cf675dea1ae7d5a85921065a2ff11308077
metzboy11 <92746504+metzboy11@users.noreply.github.com>
2022-01-20 12:05:27 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . . f f 9 6 f f . . . . . 
+    . . . . f 9 9 6 6 6 6 f . . . . 
+    . . . . f 9 6 6 6 6 6 f . . . . 
+    . . . f f 6 6 6 6 6 6 f . . . . 
+    . . . f 6 6 6 6 6 6 6 f . . . . 
+    . . f f 6 6 f 6 f 6 6 f . . . . 
+    . . f 6 6 6 6 6 6 6 8 f . . . . 
+    . . f 6 6 f 6 6 f 6 8 f . . . . 
+    . f f 6 6 6 f f 6 6 8 f f f . . 
+    . f 6 6 6 6 6 6 6 8 8 8 8 f . . 
+    f f 8 8 8 8 8 8 8 8 8 f f f . . 
+    f f f f f f f f f f f f . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 scene.setBackgroundColor(5)
