30702d7c7f838364e886293580bd6f9fa0bf59c7
metzboy11 <92746504+metzboy11@users.noreply.github.com>
2022-01-20 12:03:40 -0600

    i added a sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f f f . . . . . 
+    . . . . . . f f f 4 f f . . . . 
+    . . . . . f f 5 4 4 4 f f . . . 
+    . . . f f f 5 4 4 4 4 4 f . . . 
+    . . . f 5 5 4 4 4 4 4 4 f . . . 
+    . . f f 4 4 f 4 f 4 4 4 f . . . 
+    . . f 4 4 4 4 4 4 4 4 4 f f . . 
+    . f f 4 4 f 4 4 4 f 4 4 2 f f . 
+    . f 4 4 4 4 f f f 4 4 4 2 2 f f 
+    f f 2 2 2 4 4 4 4 4 2 2 2 2 f f 
+    f f f f 2 2 4 4 2 2 2 f f f f . 
+    . . . f f 2 2 2 2 f f f . . . . 
+    . . . . f f f f f . . . . . . . 
+    `, SpriteKind.Player)
